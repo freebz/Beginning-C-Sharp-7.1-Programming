ff5f17eb5589d8c25bebd50991c7766105444f70
freebz <freebz.dev@gmail.com>
2020-12-10 11:21:03 +0900

    2장. 개발 환경 준비

---
+++ ch02/ex2-1.Program.cs
@@ -0,0 +1,15 @@
+// 예제 2.1 기본 예제 파일 - Program.cs
+
+using System;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// 문자열 출력
+			Console.WriteLine("Hello World");
+		}
+	}
+}
